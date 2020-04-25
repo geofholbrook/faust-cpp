16dc588ee093919c0f44e9f710325179ef8aa30b
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-04-24 20:30:17 -0400

    first commit

---
+++ main/hello_world_m.cpp
@@ -0,0 +1,48 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+#include "WM8978.h"
+#include "WM8978.cpp"
+#include "PWMSiren.h"
+#include "PWMSiren.cpp"
+
+extern "C" void app_main();
+
+void app_main(void)
+{
+
+// Init audio codec
+    WM8978 wm8978;
+    wm8978.init();
+    wm8978.addaCfg(1,1);
+    wm8978.inputCfg(1,0,0);
+    wm8978.outputCfg(1,0);
+    wm8978.micGain(30);
+    wm8978.auxGain(0);
+    wm8978.lineinGain(0);
+
+    // Set gain
+    wm8978.spkVolSet(60); // [0-63]
+    
+    wm8978.hpVolSet(40,40);
+    wm8978.i2sCfg(2,0);
+    
+    // Allocate and start Faust DSP
+    PWMSiren* DSP = new PWMSiren(44100, 32);
+    DSP->start();
+    
+    // Waiting forever
+    vTaskSuspend(nullptr);
+}
